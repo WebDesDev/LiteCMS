04ddedc78ebb5f4996664d88aa79a00debebb532
Michal <msjablonsky@gmail.com>
2014-02-13 20:02:01 +0100

    The default files structure LiteCMS

---
+++ images/index.php
@@ -0,0 +1,24 @@
+<?
+/*
+ * @project LiteCMS
+ * @author	Damian Miller
+ * @company WebDes Studio 2014 ©
+ * @website http://webdes.com.pl/product,lite-cms
+ * --------------------------------------------------
+ * All rights reserved! | Wszystkie Prawa Zastrzeżone!
+ */
+?>
+<!DOCTYPE html>
+<html lang="pl_PL">
+	<head>
+		<title>Content Management System</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+	</head>
+	<body>
+		<p>
+			Content Management System
+		</p>
+	</body>
+</html>
